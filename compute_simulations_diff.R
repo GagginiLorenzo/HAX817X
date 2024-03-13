c76b1720db2883c376eda2066d5af07900c67e0b
Joseph Salmon <joseph.salmon@umontpellier.fr>
2024-03-13 14:53:53 +0100

    cosmit

--- compute_simulations_diff.R
+++ compute_simulations_diff.R
@@ -1,5 +1,5 @@
 "
-THIS IS NOT THIS ORIGINAL SCRIPT, SEE SOURCE MATERIAL 
+THIS IS NOT THE ORIGINAL SCRIPT, SEE SOURCE MATERIAL
 https://github.com/achamma723/Group_Variable_Importance
 "
 
@@ -35,10 +35,9 @@ if (!DEBUG) {
 
 ##### Running Methods #####
 
-####MODIFICATION DE LA LISTE POUR FAIRE TOURNER + VITE
+#### MODIFICATION DE LA LISTE POUR FAIRE TOURNER + VITE
 methods <- c(
   "marginal"
-
 )
 
 ##### Configuration #####
@@ -123,7 +122,7 @@ if (!DEBUG) {
 
 compute_method <- function(method,
                            index_i,
-                           n_simulations, ...) 
+                           n_simulations, ...)
   {
   print("Begin")
   cat(sprintf("%s: %i \n", method, index_i))
@@ -131,13 +130,13 @@ compute_method <- function(method,
     {
     sim_data <- generate_data(seed,...)
     print("Done loading data!")
-    
+
     #####
-    #AJOUT ICI POUR ECRIRE LES DONNES SIMULER, 
+    #AJOUT ICI POUR ECRIRE LES DONNES SIMULER,
     #seed parcour: 1:100 pour chaque rho dans rho_group, faire 1 rho a la fois pour ne pas écraser
     write.table(sim_data,file=paste(as.character(seed),"csv",sep='.'),sep=",", row.names=FALSE)
     #####
-    
+
     # Prepare the list of grouped labels
     if (list(...)$group_bool) {
       list_grps <- generate_grps(list(...)$p, list(...)$n_blocks)
